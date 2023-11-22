e51aef21e736386d2e45d2452ef5f65f7b7d7adb
Jules Dubanet <21906567@etu.unicaen.fr>
2023-11-22 14:29:55 +0100

    ajout randomStategy

--- ExplicationP4/code/src/puissance4/strategy/RandomStrategy.java
+++ ExplicationP4/code/src/puissance4/strategy/RandomStrategy.java
@@ -1,13 +1,19 @@
 package puissance4.strategy;
 
+import java.util.Random;
+
 import puissance4.model.Game;
 import puissance4.model.Player;
+import puissance4.model.State;
 
 public class RandomStrategy implements IStrategy {
 
     @Override
     public void playStrategy(Player p, Game g) {
-
+        State s = g.getCurrentState();
+        boolean[] b = s.getValidPlay();
+        int r = new Random().nextInt(b.length);
+        s.play(r);
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'playStrategy'");
     }
