d70e75bfd222d79ad74b7b9bc4d704de6cd44e39
21911419 <21911419@unicaen.fr>
2023-11-23 09:12:49 +0100

    player plays

--- ExplicationP4/code/src/puissance4/model/Player.java
+++ ExplicationP4/code/src/puissance4/model/Player.java
@@ -3,11 +3,28 @@ package puissance4.model;
 import puissance4.strategy.IStrategy;
 
 public class Player {
-    protected IStrategy strategy;
-    public int id;
+    private IStrategy strategy;
+    private int id;
+    private Game game;
 
-    public Player(int id, IStrategy strategy) {
+    public Player(int id, IStrategy strategy){
         this.id = id;
         this.strategy = strategy;
+    }
+
+    public void play(){
+        this.strategy.playStrategy(this, this.game);
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public IStrategy getStrategy(){
+        return this.strategy;
+    }
+
+    public void setGame(Game game){
+        this.game= game;
     }
 }
