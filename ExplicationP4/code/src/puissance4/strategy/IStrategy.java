cbc2ad24096c9057031ab1f3840caf0ad1a8b02f
21911419 <21911419@unicaen.fr>
2023-11-23 13:08:15 +0100

    actually changed interface to use playStrategy on a State

--- ExplicationP4/code/src/puissance4/strategy/IStrategy.java
+++ ExplicationP4/code/src/puissance4/strategy/IStrategy.java
@@ -1,11 +1,17 @@
 package puissance4.strategy;
 
 import puissance4.model.Player;
+import puissance4.model.State;
 import puissance4.model.Game;
 
 /**
  * Interface for players' strategies.
  */
 public interface IStrategy {
-    public int playStrategy(State state); /** TODO: Fix implementations */
+    /**
+     * Strategie de jeu
+     * @param state l'état sur lequel appliquer la stratégies
+     * @return un coup à jouer
+     */
+    public int playStrategy(State state); 
 }
