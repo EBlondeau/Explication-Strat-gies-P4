8d4723000c1a6bc730fc4439773f79b8defba2bf
21911419 <21911419@unicaen.fr>
2023-11-27 10:18:43 +0100

    user input strategy

--- ExplicationP4/code/src/puissance4/model/Main.java
+++ ExplicationP4/code/src/puissance4/model/Main.java
@@ -2,10 +2,13 @@ package puissance4.model;
 
 import java.util.Arrays;
 
+import puissance4.strategy.RandomStrategy;
+import puissance4.strategy.UserInputStrategy;
+
 public class Main {
     public static void main(String[] args) {
 
-        Game game= new Game();
+        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()));
 
         while(!game.getCurrentState().isDone()){
             game.getCurrentState().printStateFull();
---
+++ ExplicationP4/code/src/puissance4/strategy/UserInputStrategy.java
@@ -0,0 +1,18 @@
+package puissance4.strategy;
+
+import java.util.Scanner;
+
+import puissance4.model.State;
+
+public class UserInputStrategy implements IStrategy {
+
+    @Override
+    public int playStrategy(State state) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Type move: ");
+        int move= sc.nextInt();
+        return move;
+
+    }
+    
+}
