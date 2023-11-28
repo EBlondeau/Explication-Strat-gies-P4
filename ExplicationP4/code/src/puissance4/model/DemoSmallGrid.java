182ccb181b8ab9950ba2b1770e2d80334c04fd44
21911419 <21911419@unicaen.fr>
2023-11-28 14:54:13 +0100

    working demo on a small grid

--- ExplicationP4/code/src/puissance4/model/DemoSmallGrid.java
+++ ExplicationP4/code/src/puissance4/model/DemoSmallGrid.java
@@ -1,5 +1,6 @@
 package puissance4.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -10,14 +11,18 @@ import puissance4.strategy.algorithms.Negamax;
 public class DemoSmallGrid {
     public static void main(String[] args) {
 
-        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new RandomStrategy()),4, 5);
+        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()),4, 5);
         Negamax nega= new Negamax();
-        Arrays.toString(nega.getAllScore(game.getCurrentState()));
+        //String scores= new ArrayList
+        //int score= nega.getScore(game.getCurrentState(), 0);
+        
         while(!game.getCurrentState().isDone()){
             game.getCurrentState().printStateFull();
             int move= game.getCurrentState().getCurrentPlayer().play();
             System.out.println("Played move:" + move);
             game.getCurrentState().play(move);
+            String scores= Arrays.toString(nega.getAllScore(game.getCurrentState()));
+            System.out.println(scores);
             
         }
 
