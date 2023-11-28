7c1ee70e3b0db84432ea84f0669d86b05a8ade04
Jules Dubanet <21906567@etu.unicaen.fr>
2023-11-28 14:22:37 +0100

    ajout depth a nigamax

--- ExplicationP4/code/src/puissance4/strategy/algorithms/Negamax.java
+++ ExplicationP4/code/src/puissance4/strategy/algorithms/Negamax.java
@@ -5,58 +5,69 @@ import java.util.ArrayList;
 import puissance4.model.State;
 
 public class Negamax {
+    private int depth;
+    public static final int DEFAULT_DEPTH = 4;
 
-    public int getBestMove(State state){
-        int[] scores= this.getAllScore(state);
-        int max=0;
-        int index=-1;
-        for(int i=0; i<scores.length; i++){
-            if(scores[i]>max){
-                max= scores[i];
-                index=i;
-            } 
+    public Negamax(int depth) {
+        this.depth = depth;
+    }
+
+    public Negamax() {
+        this.depth = DEFAULT_DEPTH;
+    }
+
+    public int getBestMove(State state) {
+        int[] scores = this.getAllScore(state);
+        int max = -1;
+        int index = -1;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+                index = i;
+            }
         }
         return index;
     }
 
-    public int[] getAllScore(State state){
-        ArrayList<Integer> validPlays= state.getValidPlay();
-        int[] res= new int[state.getGame().getWidth()];
-        for(int i=0; i<state.getGame().getWidth();i++){
-            if(validPlays.contains(i)){
-                res[i]= this.getScore(state, i);
-            }
-            else res[i]=0;
+    public int[] getAllScore(State state) {
+        ArrayList<Integer> validPlays = state.getValidPlay();
+        int[] res = new int[state.getGame().getWidth()];
+        for (int i = 0; i < state.getGame().getWidth(); i++) {
+            if (validPlays.contains(i)) {
+                res[i] = this.getScore(state, i);
+            } else
+                res[i] = 0;
         }
 
         return res;
     }
 
-    public int getScore(State state, int move){
-        State nextState= state.play(move, false);
-        return this.negamax(nextState);
+    public int getScore(State state, int move) {
+        State nextState = state.play(move, false);
+        return this.negamax(nextState, this.depth);
     }
 
-    public int negamax(State state){
-        int gWidth= state.getGame().getWidth();
-        int gHeight= state.getGame().getHeight();
-        //Check for a draw
-        if(state.isDone()){
+    public int negamax(State state, int depth) {
+        int gWidth = state.getGame().getWidth();
+        int gHeight = state.getGame().getHeight();
+        // Check for a draw
+        if (state.isDone() || depth == 0) {
             return 0;
         }
-        for(int move:state.getValidPlay()){
-            //System.out.println("MOVE AAA: " + move);
-            if(state.isWinningMove(move)){
-                return gWidth*gHeight+1 -state.nbMoves()/2;
+        for (int move : state.getValidPlay()) {
+            // System.out.println("MOVE AAA: " + move);
+            if (state.isWinningMove(move)) {
+                return gWidth * gHeight + 1 - state.nbMoves() / 2;
             }
         }
 
-        int bestScore = -(gWidth*gHeight);
+        int bestScore = -(gWidth * gHeight);
 
-        for(int move: state.getValidPlay()){
-            State nextState= state.play(move, false);
-            int score= -(negamax(nextState));
-            if(score>bestScore) bestScore=score;
+        for (int move : state.getValidPlay()) {
+            State nextState = state.play(move, false);
+            int score = -(negamax(nextState, depth - 1));
+            if (score > bestScore)
+                bestScore = score;
         }
 
         return bestScore;
