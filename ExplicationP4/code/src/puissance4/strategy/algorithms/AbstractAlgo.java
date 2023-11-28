83558ef08d09256d6b8f8ee813164a1904f3a6ca
21911419 <21911419@unicaen.fr>
2023-11-28 15:05:34 +0100

    fix getBestMove(), implement abstract class for algorithms

---
+++ ExplicationP4/code/src/puissance4/strategy/algorithms/AbstractAlgo.java
@@ -0,0 +1,55 @@
+package puissance4.strategy.algorithms;
+
+import java.util.ArrayList;
+
+import puissance4.model.State;
+
+public abstract class AbstractAlgo {
+
+    private int depth;
+    public static final int DEFAULT_DEPTH = 10;
+
+    public AbstractAlgo(int depth) {
+        this.depth = depth;
+    }
+
+    public AbstractAlgo() {
+        this.depth = DEFAULT_DEPTH;
+    }
+
+    public abstract int algorithm(State state, int depth);
+
+    public int getBestMove(State state) {
+        int[] scores = this.getAllScore(state);
+        int max = -(state.getGame().getWidth()*state.getGame().getHeight());
+        int index = -1;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public int[] getAllScore(State state) {
+        ArrayList<Integer> validPlays = state.getValidPlay();
+        int[] res = new int[state.getGame().getWidth()];
+        for (int i = 0; i < state.getGame().getWidth(); i++) {
+            if (validPlays.contains(i)) {
+                res[i] = this.getScore(state, i);
+            } 
+            else{
+                res[i] = 0;
+            }
+                
+        }
+
+        return res;
+    }
+
+    public int getScore(State state, int move) {
+        State nextState = state.play(move, false);
+        return -this.algorithm(nextState, this.depth);
+    }
+}
--- ExplicationP4/code/src/puissance4/strategy/algorithms/Negamax.java
+++ ExplicationP4/code/src/puissance4/strategy/algorithms/Negamax.java
@@ -4,53 +4,9 @@ import java.util.ArrayList;
 
 import puissance4.model.State;
 
-public class Negamax {
-    private int depth;
-    public static final int DEFAULT_DEPTH = 10;
+public class Negamax extends AbstractAlgo {
 
-    public Negamax(int depth) {
-        this.depth = depth;
-    }
-
-    public Negamax() {
-        this.depth = DEFAULT_DEPTH;
-    }
-
-    public int getBestMove(State state) {
-        int[] scores = this.getAllScore(state);
-        int max = -1;
-        int index = -1;
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] > max) {
-                max = scores[i];
-                index = i;
-            }
-        }
-        return index;
-    }
-
-    public int[] getAllScore(State state) {
-        ArrayList<Integer> validPlays = state.getValidPlay();
-        int[] res = new int[state.getGame().getWidth()];
-        for (int i = 0; i < state.getGame().getWidth(); i++) {
-            if (validPlays.contains(i)) {
-                res[i] = this.getScore(state, i);
-            } 
-            else{
-                res[i] = 0;
-            }
-                
-        }
-
-        return res;
-    }
-
-    public int getScore(State state, int move) {
-        State nextState = state.play(move, false);
-        return -this.negamax(nextState, this.depth);
-    }
-
-    public int negamax(State state, int depth) {
+    public int algorithm(State state, int depth) {
         int gWidth = state.getGame().getWidth();
         int gHeight = state.getGame().getHeight();
         // Check for a draw
@@ -69,7 +25,7 @@ public class Negamax {
         //System.out.println(bestScore);
         for (int move : state.getValidPlay()) {
             State nextState = state.play(move, false);
-            int score = -negamax(nextState, depth - 1);
+            int score = -algorithm(nextState, depth - 1);
             //System.out.print(score<0 ? score : "");
             if (score > bestScore)
                 bestScore = score;
