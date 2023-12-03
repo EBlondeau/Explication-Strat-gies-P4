95a22b9065bba3508730325c26fc7b5d5d101380
21911419 <21911419@unicaen.fr>
2023-12-03 13:04:48 +0100

    smallGrid demo uses alphabeta

--- ExplicationP4/code/src/puissance4/model/DemoSmallGrid.java
+++ ExplicationP4/code/src/puissance4/model/DemoSmallGrid.java
@@ -6,13 +6,16 @@ import java.util.Random;
 
 import puissance4.strategy.RandomStrategy;
 import puissance4.strategy.UserInputStrategy;
+import puissance4.strategy.algorithms.AbstractAlgo;
+import puissance4.strategy.algorithms.Alphabeta;
 import puissance4.strategy.algorithms.Negamax;
 
 public class DemoSmallGrid {
     public static void main(String[] args) {
 
-        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()),4, 5);
-        Negamax nega= new Negamax();
+        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()),6, 7);
+        AbstractAlgo nega= new Negamax();
+        AbstractAlgo alpha= new Alphabeta();
         //String scores= new ArrayList
         //int score= nega.getScore(game.getCurrentState(), 0);
         
@@ -21,7 +24,8 @@ public class DemoSmallGrid {
             int move= game.getCurrentState().getCurrentPlayer().play();
             System.out.println("Played move:" + move);
             game.getCurrentState().play(move);
-            String scores= Arrays.toString(nega.getAllScore(game.getCurrentState()));
+            //String scores= Arrays.toString(nega.getAllScore(game.getCurrentState()));
+            String scores= Arrays.toString(alpha.getAllScore(game.getCurrentState()));
             System.out.println(scores);
             
         }
