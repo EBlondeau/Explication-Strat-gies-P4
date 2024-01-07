7ce7b1fcb17d6662c3007a2b9042b4a4b1924d64
Emilien Blondeau <21911419@etu.unicaen.fr>
2024-01-07 12:43:46 +0100

    comments

--- ExplicationP4/code/src/puissance4/strategy/algorithms/AbstractAlgo.java
+++ ExplicationP4/code/src/puissance4/strategy/algorithms/AbstractAlgo.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 
 import puissance4.model.State;
 
+/**
+ * Classe abstraite représentant un Algoritme pour le puissance 4
+ * Consiste en une fonction abstraite Algoritme qui permet d'implementer l'algo souhaité
+ * Ainsi que de fonction permettant d'obtenir les scores retournés par cet algo
+ */
 public abstract class AbstractAlgo {
 
     private int depth;
-    public static final int DEFAULT_DEPTH = 25;
+    public static final int DEFAULT_DEPTH = 50;
 
     public AbstractAlgo(int depth) {
         this.depth = depth;
@@ -17,8 +22,19 @@ public abstract class AbstractAlgo {
         this.depth = DEFAULT_DEPTH;
     }
 
+    /**
+     * Fonction implémentant un algoritme
+     * @param state l'état sur lequel appliquer l'algoritme
+     * @param depth la profondeur souhaitée
+     * @return le score maximum trouvé
+     */
     public abstract int algorithm(State state, int depth);
 
+    /**
+     * Fonction retournant le meilleur coup pour un état donné
+     * @param state l'état donné
+     * @return le meilleur coup pour cet état
+     */
     public int getBestMove(State state) {
         int[] scores = this.getAllScore(state);
         int max = -(state.getGame().getWidth() * state.getGame().getHeight());
@@ -32,6 +48,11 @@ public abstract class AbstractAlgo {
         return index;
     }
 
+    /**
+     * Fonction retournant les scores de chaques coups possibles pour un état donné
+     * @param state l'état donné
+     * @return une liste des scores
+     */
     public int[] getAllScore(State state) {
         ArrayList<Integer> validPlays = state.getValidPlay();
         int[] res = new int[state.getGame().getWidth()];
@@ -47,6 +68,12 @@ public abstract class AbstractAlgo {
         return res;
     }
 
+    /**
+     * Fonction retournant le score pour un coups donné sur un état donné
+     * @param state l'état donné
+     * @param move le coup correspondant
+     * @return le score de ce coup
+     */
     public int getScore(State state, int move) {
         State nextState = state.play(move, false);
         return -this.algorithm(nextState, this.depth);
