215e71f6bd5e6664244daafe80d734f7a99a8579
Jules Dubanet <21906567@etu.unicaen.fr>
2024-03-25 12:53:26 +0100

    transposition table a taille limitées

--- ExplicationP4/code/src/puissance4/model/Main.java
+++ ExplicationP4/code/src/puissance4/model/Main.java
@@ -8,16 +8,32 @@ import puissance4.model.strategy.UserInputStrategy;
 public class Main {
     public static void main(String[] args) {
 
-        Game game= new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()));
+        Game game = new Game(new Player(1, new UserInputStrategy()), new Player(2, new UserInputStrategy()));
 
-        while(!game.getCurrentState().isDone()){
-            game.getCurrentState().printStateFull();
-            int move= game.getCurrentState().getCurrentPlayer().play();
-            System.out.println("Played move:" + move);
-            game.getCurrentState().play(move);
-            System.out.println(game.getCurrentState().getValidPlay());
-        }
+        /*
+         * while(!game.getCurrentState().isDone()){
+         * game.getCurrentState().printStateFull();
+         * int move= game.getCurrentState().getCurrentPlayer().play();
+         * System.out.println("Played move:" + move);
+         * game.getCurrentState().play(move);
+         * System.out.println(game.getCurrentState().getValidPlay());
+         * }
+         * 
+         * game.getCurrentState().printStateFull();
+         */
+        TranspositionTable t = new TranspositionTable();
+        t.put(1, 1);
+        t.put(2, 1);
+        t.put(3, 1);
+        t.put(4, 1);
+        t.put(5, 1);
+        System.out.println(t.get(5));
+        t.put(6, 0);
+        System.out.println(t.get(2));
+        System.out.println(t.size());
+        t.put(7, 0);
+        System.out.println(t.get(14));
+        System.out.println(t.size());
 
-        game.getCurrentState().printStateFull();
     }
 }
--- ExplicationP4/code/src/puissance4/model/TranspositionTable.java
+++ ExplicationP4/code/src/puissance4/model/TranspositionTable.java
@@ -1,20 +1,37 @@
 package puissance4.model;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class TranspositionTable {
-
-    public HashMap<Integer, Integer> table;
+    private static final int MAX_ENTRIES = 5;
+    public LinkedHashMap<Integer, Integer> table;
 
     public TranspositionTable() {
+        this.table = new LinkedHashMap<Integer, Integer>(MAX_ENTRIES, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > MAX_ENTRIES;
+            }
+        };
     }
 
     public void put(int key, int val) {
         this.table.put(key, val);
     };
 
-    public int get(int key) {
-        return this.table.get(key);
+    public Integer get(int key) {
+        if (this.table.containsKey(key)) {
+            return this.table.get(key);
+        } else {
+            return null;
+        }
 
     }
+
+    public int size() {
+        return this.table.size();
+    }
+
 }
